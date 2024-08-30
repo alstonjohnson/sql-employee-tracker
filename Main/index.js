92a1f8e4e0c170b49d4da22ddaf3b421615f87ed
alstonjohnson <johnson.alston@icloud.com>
2024-08-30 14:44:22 -0500

    started prompt

--- Main/index.js
+++ Main/index.js
@@ -1,3 +1,10 @@
 
 const inquirer = require('inquirer');
 const postgres = require('pg'); 
+
+
+const questions = function () {
+    inquirer.createPromptModule([{
+        type: 'list',
+    }])
+}
