2a837297770cc6642f3f4af5ffd0fa5bac02ee80
alstonjohnson <johnson.alston@icloud.com>
2024-08-30 16:14:41 -0500

    adding a role

--- Main/index.js
+++ Main/index.js
@@ -1,7 +1,7 @@
 
 const inquirer = require('inquirer');
 const postgres = require('pg'); 
-const database = require('./db/connection')
+const database = require('./database/connection')
 
 
 const tracker = function () {
@@ -49,6 +49,60 @@ const tracker = function () {
                     tracker();
                 });
             })
-        } 
-    })
-}
+        } else if (answers.prompt === 'Add A Role') {
+            database.query(`SELECT * FROM department`, (result) => {
+
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'role',
+                        message: 'What is the name of the role?',
+                        validate: roleInput => {
+                            if (roleInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Role!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'What is the salary of the role?',
+                        validate: salaryInput => {
+                            if (salaryInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Salary!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department does the role belong to?',
+                        choices: () => {
+                            const array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].name);
+                            }
+                            return array;
+                        }
+                    }
+                ]).then((answers) => {
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].name === answers.department) {
+                            const department = result[i];
+                        }
+                    }
+
+                    database.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [answers.role, answers.salary, department.id], (result) => {
+                        console.log(`Added ${answers.role} to the database.`)
+                        tracker();
+                    });
+                })
+            });
+    }})
+};
