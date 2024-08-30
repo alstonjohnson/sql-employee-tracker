c2b94b37c2d046fa9d1ee8fc46d7c91b16e1d98a
alstonjohnson <johnson.alston@icloud.com>
2024-08-30 14:55:00 -0500

    implemented the initial prompt

--- Main/index.js
+++ Main/index.js
@@ -3,8 +3,11 @@ const inquirer = require('inquirer');
 const postgres = require('pg'); 
 
 
-const questions = function () {
+const tracker = function () {
     inquirer.createPromptModule([{
         type: 'list',
+        name: 'prompt',
+        message: 'What would you like to do?',
+        choices: ['View all dpartments', 'View all roles', 'view all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an empolyee role']
     }])
 }
