1281478142b10f18f8121f297add476b522fb800
alstonjohnson <johnson.alston@icloud.com>
2024-08-31 13:08:28 -0500

    inserted into role table

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -6,4 +6,14 @@ VALUES  ('Finance'),
         ('Procurement'),
         ('Operations'),;
 
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Staff Accountant', 70000, 1),
+        ('Senior Accountant', 90000, 1),
+        ('Social Media Manager', 95000, 2), 
+        ('Software Engineer', 100000, 3),
+        ('Procurement Specialist', 70000, 4), 
+        ('Chief Operations Officer', 150000, 5),
+        ('Full Stack Developer', 100000, 3),
+        ('Data Analyst', 110000, 3);
+
     
