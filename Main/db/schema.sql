cc5b98618146022dccb1d921f7223ec43459ea7f
alstonjohnson <johnson.alston@icloud.com>
2024-09-01 12:46:55 -0500

    updated integer

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -5,19 +5,19 @@ CREATE DATABASE employee_database;
 \c employee_database;
 
 CREATE TABLE department (
-    id INTEGER SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INTEGER SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER
 );
 
 CREATE TABLE employee (
-    id INTEGER SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
