909e76558e597c92ea13cc12f9847bc412cb55dc
alstonjohnson <johnson.alston@icloud.com>
2024-09-01 15:03:21 -0500

    updated code

--- Main/server.js
+++ Main/server.js
@@ -14,21 +14,21 @@ const tracker = function () {
             database.query(`SELECT * FROM department`, (err, result) => {
                 // if (err) throw err;
                 console.log("Viewing All Departments: ");
-                console.table(result.rows);
+                console.table(result);
                 tracker();
             });
         } else if (answers.prompt === 'View all roles') {
             database.query(`SELECT * FROM role`, (err, result) => {
                 // if (err) throw err;
                 console.log("Viewing All Roles: ");
-                console.table(result.rows);
+                console.table(result);
                 tracker();
             });
         } else if (answers.prompt === 'View all employees') {
             database.query(`SELECT * FROM employee`, (err, result) => {
                 // if (err) throw err;
                 console.log("Viewing All Employees: ");
-                console.table(result.rows);
+                console.table(result);
                 tracker();
             });
         } else if (answers.prompt === 'Add a department') {
@@ -45,7 +45,7 @@ const tracker = function () {
                     }
                 }
             }]).then((departmentAnswers) => {
-                database.query(`INSERT INTO department (name) VALUES (?)`, [departmentAnswers.department], (err, result) => {
+                database.query(`INSERT INTO department (name) VALUES ($1)`, [departmentAnswers.department], (err, result) => {
                     // if (err) throw err;
                     console.log(`Added ${departmentAnswers.department} to the database.`);
                     tracker();
@@ -86,12 +86,12 @@ const tracker = function () {
                         name: 'department',
                         message: 'Which department does the role belong to?',
                         choices: () => {
-                            return result.map(department => department.name);
+                            return result.rows.map(department => department.name);
                         }
                     }
                 ]).then((roleAnswers) => {
-                    const department = result.find(dep => dep.name === roleAnswers.department);
-                    database.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [roleAnswers.role, roleAnswers.salary, department.id], (err, result) => {
+                    const department = result.rows.find(dep => dep.name === roleAnswers.department);
+                    database.query(`INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)`, [roleAnswers.role, roleAnswers.salary, department.id], (err, result) => {
                         // if (err) throw err;
                         console.log(`Added ${roleAnswers.role} to the database.`);
                         tracker();
@@ -133,7 +133,7 @@ const tracker = function () {
                         name: 'role',
                         message: 'What is the employee\'s role?',
                         choices: () => {
-                            return result.map(role => role.title);
+                            return result.rows.map(role => role.title);
                         }
                     },
                     {
@@ -150,97 +150,24 @@ const tracker = function () {
                         }
                     }
                 ]).then((employeeAnswers) => {
-                    const role = result.find(r => r.title === employeeAnswers.role);
-                    database.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [employeeAnswers.firstName, employeeAnswers.lastName, role.id, employeeAnswers.manager], (err, result) => {
+                    const role = result.rows.find(r => r.title === employeeAnswers.role);
+                    database.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`, [employeeAnswers.firstName, employeeAnswers.lastName, role.id, employeeAnswers.manager], (err, result) => {
                         // if (err) throw err;
                         console.log(`Added ${employeeAnswers.firstName} ${employeeAnswers.lastName} to the database.`);
                         tracker();
                     });
                 });
             });
-    } else if (answers.prompt === 'Update An Employee Role') {if (answers.prompt === 'Add An Employee') {
-        database.query(`SELECT * FROM employee, role`, (result) => {
-
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'firstName',
-                    message: 'What is the employees first name?',
-                    validate: firstNameInput => {
-                        if (firstNameInput) {
-                            return true;
-                        } else {
-                            console.log('Please Add A First Name!');
-                            return false;
-                        }
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'lastName',
-                    message: 'What is the employees last name?',
-                    validate: lastNameInput => {
-                        if (lastNameInput) {
-                            return true;
-                        } else {
-                            console.log('Please Add A Salary!');
-                            return false;
-                        }
-                    }
-                },
-                {
-                    type: 'list',
-                    name: 'role',
-                    message: 'What is the employees role?',
-                    choices: () => {
-                        const array = [];
-                        for (var i = 0; i < result.length; i++) {
-                            array.push(result[i].title);
-                        }
-                        const newArray = [...new Set(array)];
-                        return newArray;
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'manager',
-                    message: 'Who is the employees manager?',
-                    validate: managerInput => {
-                        if (managerInput) {
-                            return true;
-                        } else {
-                            console.log('Please Add A Manager!');
-                            return false;
-                        }
-                    }
-                }
-            ]).then((answers) => {
-                for (var i = 0; i < result.length; i++) {
-                    if (result[i].title === answers.role) {
-                        const role = result[i];
-                    }
-                }
-
-                database.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [answers.firstName, answers.lastName, role.id, answers.manager.id], (result) => {
-                    console.log(`Added ${answers.firstName} ${answers.lastName} to the database.`)
-                    tracker();
-                });
-            })
-        });
-    }
-        database.query(`SELECT * FROM employee, role`, (result) => {
-
+    } else if (answers.prompt === 'Update an employee role') {
+        database.query(`SELECT * FROM employee, role`, (err, result) => {
+            // if (err) throw err;
             inquirer.prompt([
                 {
                     type: 'list',
                     name: 'employee',
-                    message: 'Which employees role do you want to update?',
+                    message: 'Which employee\'s role do you want to update?',
                     choices: () => {
-                        const array = [];
-                        for (var i = 0; i < result.length; i++) {
-                            array.push(result[i].last_name);
-                        }
-                        const employeeArray = [...new Set(array)];
+                        const employeeArray = [...new Set(result.rows.map(row => row.last_name))];
                         return employeeArray;
                     }
                 },
@@ -249,34 +176,23 @@ const tracker = function () {
                     name: 'role',
                     message: 'What is their new role?',
                     choices: () => {
-                        const array = [];
-                        for (var i = 0; i < result.length; i++) {
-                            array.push(result[i].title);
-                        }
-                        const newArray = [...new Set(array)];
-                        return newArray;
+                        const roleArray = [...new Set(result.rows.map(row => row.title))];
+                        return roleArray;
                     }
                 }
             ]).then((answers) => {
-                for (var i = 0; i < result.length; i++) {
-                    if (result[i].last_name === answers.employee) {
-                        const name = result[i];
-                    }
-                }
+                const name = result.rows.find(row => row.last_name === answers.employee);
+                const role = result.rows.find(row => row.title === answers.role);
 
-                for (var i = 0; i < result.length; i++) {
-                    if (result[i].title === answers.role) {
-                        const role = result[i];
-                    }
-                }
-
-                database.query(`UPDATE employee SET ? WHERE ?`, [{ role_id: role }, { last_name: name }], (result) => {
-                    console.log(`Updated ${answers.employee} role to the database.`)
+                database.query(`UPDATE employee SET role_id = $1 WHERE last_name = $2`, [role.id, name.last_name], (err, result) => {
+                    // if (err) throw err;
+                    console.log(`Updated ${answers.employee}'s role in the database.`);
                     tracker();
                 });
-            })
+            });
         });
- } });
+    }
+    });
 };
 
 tracker();
