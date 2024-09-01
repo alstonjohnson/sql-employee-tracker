22dfcf3c1e42958f68582ef9cb94c9041e69f786
alstonjohnson <johnson.alston@icloud.com>
2024-09-01 15:44:35 -0500

    updated seed.sql

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -9,21 +9,19 @@ VALUES  ('Finance'),
 INSERT INTO role (title, salary, department_id)
 VALUES  ('Staff Accountant', 70000, 1),
         ('Senior Accountant', 90000, 1),
-        ('Social Media Manager', 95000, 2), 
+        ('Social Media Manager', 95000, 2),
         ('Software Engineer', 100000, 3),
-        ('Procurement Specialist', 70000, 4), 
+        ('Procurement Specialist', 70000, 4),
         ('Chief Operations Officer', 150000, 5),
         ('Full Stack Developer', 100000, 3),
         ('Data Analyst', 110000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Nasery', 'Athear', 1, 2),
-        ('Stark', 'Skip', 2, null),
-        ('Martin', 'Carter', 3, null),
+        ('Stark', 'Skip', 2, NULL),
+        ('Martin', 'Carter', 3, NULL),
         ('Wegman', 'Michael', 4, 7),
         ('Maza', 'Rowena', 5, 6),
-        ('Mann', 'Mike', 6, null),
-        ('Johnson', 'Alston', 7, null),
-        ('Hazlewood', 'Amber', 8, null);
-
-    
+        ('Mann', 'Mike', 6, NULL),
+        ('Johnson', 'Alston', 7, NULL),
+        ('Hazlewood', 'Amber', 8, NULL);
