f3611ddc78259e1458732b973b72f34c37f19bb4
alstonjohnson <johnson.alston@icloud.com>
2024-09-01 12:44:08 -0500

    changed posrgres wording

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,22 +1,23 @@
 
 DROP DATABASE IF EXISTS employee_database;
 CREATE DATABASE employee_database;
-USE employee_database;
+
+\c employee_database;
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER SERIAL PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER SERIAL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
